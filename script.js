0c4d8ade2986da99daaa2982cdd01b957bfae96d
EliteMan7 <elyasahmedx@gmail.com>
2024-02-04 03:08:33 -0800

    First Stage

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".burger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+} 
+
+document.getElementById('myGif').addEventListener('mouseenter', function() {
+  var image = document.getElementById('myGif');
+  var imageUrl = image.src;
+  image.src = "";
+  image.src = imageUrl;
+});
